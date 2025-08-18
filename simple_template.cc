94d2d2d6532c16b7eeae4e5d9f7d4f292fa5c3f1
Ayush <aayush.sang@gmail.com>
2025-08-19 00:32:58 +0530

    updt

---
+++ simple_template.cc
@@ -0,0 +1,35 @@
+/**
+ *    Created:
+ *    Time Limit:
+ *    Memory Limit:
+ **/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio()                 \
+    ios::sync_with_stdio(false); \
+    cin.exceptions(cin.failbit); \
+    cin.tie(nullptr);            \
+    cout.tie(nullptr);
+
+using ll = long long;
+using ull = unsigned long long;
+using vl = vector<ll>;
+using vvl = vector<vl>;
+using vi = vector<int>;
+using vvi = vector<vi>;
+
+void solve() {}
+
+int main() {
+    fastio();
+
+    int t = 1;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+
+    return 0;
+}
